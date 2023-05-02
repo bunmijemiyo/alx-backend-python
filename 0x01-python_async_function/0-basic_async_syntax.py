47a5b344ef1effa28724e4ab648f61fdddfc5127
olubunmi <bjemiyor@gmail.com>
2023-05-02 16:24:09 +0100

    Readme modified

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Basics Of Async """
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """ sleeps for random number of seconds """
+    wait_time = random.random() * max_delay
+    await asyncio.sleep(wait_time)
+    return wait_time
---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Multiple coroutines. """
+from typing import List
+import asyncio
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """ returns list of all delays """
+    wait_list = await asyncio.gather(
+        *tuple(map(lambda _: wait_random(max_delay), range(n)))
+    )
+    return sorted(wait_list)
